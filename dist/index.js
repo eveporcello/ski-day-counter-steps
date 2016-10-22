09184206c3886f027ea84e191e97978c299b6f22
eveporcello <eve@moonhighway.com>
2016-10-22 16:45:46 -0700

    adds JSX

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,11 @@
-const { createElement } = React
+// const { createElement } = React
 const { render } = ReactDOM
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'verdana'
-}
-
-
-const title = createElement(
-    'h1',
-    {id:'title', className: 'header', style: style},
-    'Hello World!'
-)
-
 render(
-    title,
+    <h1 id='title'
+        className='header'
+        style={{backgroundColor: 'orange', color: 'white' }}>
+        Hello World!
+    </h1>,
     document.getElementById('react-container')
 )
