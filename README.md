756814a3ebcc8ed812a1459ae396d4c628fa2c87
eveporcello <eve@moonhighway.com>
2016-10-22 16:38:14 -0700

    Adds first files

---
+++ README.md
@@ -0,0 +1,13 @@
+02_01 : Pure React
+=========
+
+* Introduce React Elements
+* Create Dist
+* Create index.js
+* React.createElement
+* We can destructure this.
+* ReactDOM is the set of instructions used to build the DOM
+* Create index.html
+* sudo npm install -g httpster
+* httpster -d dist -p 3000
+
---
+++ dist/index.js
@@ -0,0 +1,20 @@
+const { createElement } = React
+const { render } = ReactDOM
+
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'
+}
+
+
+const title = createElement(
+    'h1',
+    {id:'title', className: 'header', style: style},
+    'Hello World!'
+)
+
+render(
+    title,
+    document.getElementById('react-container')
+)
