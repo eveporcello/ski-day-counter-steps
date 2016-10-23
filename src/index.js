b9f333e6407f9e4efd809d6d472dee8b9537c264
eveporcello <eve@moonhighway.com>
2016-10-22 17:57:19 -0700

    2.5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// const { createElement } = React
-const { render } = ReactDOM
+import React from 'react'
+import { render } from 'react-dom'
 
 render(
     <h1 id='title'
         className='header'
-        style={{backgroundColor: 'orange', color: 'white' }}>
+        style={{backgroundColor: 'purple', color: 'yellow' }}>
         Hello World!
     </h1>,
     document.getElementById('react-container')
